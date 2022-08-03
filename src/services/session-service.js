d5bb298a4edabaac396375e981ebd837c3e3a5de
emachipana <emachipana0102@gmail.com>
2022-08-02 19:50:18 -0500

    ADD: get user

--- src/services/session-service.js
+++ src/services/session-service.js
@@ -4,11 +4,21 @@ import apiFetch from "./api-fetch";
 export async function login(credentials) {
   const { token } = await apiFetch("login", { body: credentials });
   sessionStorage.setItem(token_key, token);
+  
+  return token;
+}
+
+export function getUser() {
+  const token = sessionStorage.getItem(token_key);
+
+  return Promise.resolve(token);
 }
 
 export async function signup(data) {
   const { token } = await apiFetch("signup", { body: data });
   sessionStorage.setItem(token_key, token);
+
+  return token;
 }
 
 export async function logout() {
