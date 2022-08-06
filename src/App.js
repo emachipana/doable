a31918eadc8c1abdb2cbe7295589d77840893120
emachipana <emachipana0102@gmail.com>
2022-08-05 22:45:00 -0500

    FIX: move header to main app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import AuthenticatedApp from "./AuthenticatedApp";
+import Header from "./components/Header";
 import { useAuth } from "./context/auth-context";
 import UnauthenticatedApp from "./UnauthenticatedApp";
 
 function App() {
   const { user } = useAuth();
 
-  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />
+  return (
+    <>
+      <Header
+        isAuth={!!user}
+      />
+      { user ? <AuthenticatedApp /> : <UnauthenticatedApp /> }
+    </>
+  )
 }
 
 export default App;
--- src/pages/session-page.js
+++ src/pages/session-page.js
@@ -1,20 +1,13 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "reactstrap";
-import Header from "../components/Header";
 import SessionForm from "../components/SessionForm";
-import { useAuth } from "../context/auth-context";
 import { Container } from "./ui";
 
 function SessionPage() {
-  const { user } = useAuth();
   const location = useLocation();
   const navigate = useNavigate();
 
   return (
-    <>
-      <Header 
-        isAuth={!!user}
-      />
       <Container>
         <SessionForm
           location={location.pathname}
@@ -28,7 +21,6 @@ function SessionPage() {
           {location.pathname === "/login" ? "Signup" : "Login"}
         </Button>
       </Container>
-    </>
   );
 }
 
