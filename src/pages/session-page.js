4943d91a6641f2d65cc9e3b905d4205edb09c0c9
emachipana <emachipana0102@gmail.com>
2022-08-06 17:44:53 -0500

    ADD: set error when click the button

--- src/pages/session-page.js
+++ src/pages/session-page.js
@@ -1,11 +1,13 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "reactstrap";
 import SessionForm from "../components/SessionForm";
+import { useAuth } from "../context/auth-context";
 import { Container } from "./ui";
 
 function SessionPage() {
   const location = useLocation();
   const navigate = useNavigate();
+  const { setError } = useAuth();
 
   return (
       <Container>
@@ -16,7 +18,10 @@ function SessionPage() {
           style={{margin: "1rem 0"}}
           color="link"
           tag="a"
-          onClick={() => navigate(location.pathname === "/login" ? "/signup" : "/login")}
+          onClick={() => {
+            navigate(location.pathname === "/login" ? "/signup" : "/login");
+            setError(null);
+          }}
         >
           {location.pathname === "/login" ? "Signup" : "Login"}
         </Button>
