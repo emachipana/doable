3fa73fabda946d836b58384cc414501377999f1a
emachipana <emachipana0102@gmail.com>
2022-08-06 17:44:20 -0500

    ADD: section & division in UI

--- src/pages/ui.js
+++ src/pages/ui.js
@@ -1,11 +1,28 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-  max-width: 350px;
+  max-width: 420px;
   margin: 0 auto;
   height: 70vh;
   padding: 1rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
+`;
+
+export const Section = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  margin: 1rem 0;
+  justify-content: space-between;
+  gap: 0.5rem;
+`;
+
+export const Division = styled.div`
+  max-width: 230px;
+  min-width: 220px;
+  display: flex;
+  gap: 6px;
+  align-items: center;
 `;
