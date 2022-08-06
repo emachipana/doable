c31ddfa28d1c98b8eefa9eb9be50b783eef17e84
emachipana <emachipana0102@gmail.com>
2022-08-06 11:05:02 -0500

    ADD: header component

---
+++ src/components/Loader/styles.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
