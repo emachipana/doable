29c0e025bf77526cfea03c9d26d45095dcd8cc3c
emachipana <emachipana0102@gmail.com>
2022-08-09 11:18:09 -0500

    ADD: functionalities to task card

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -2,14 +2,47 @@ import { FormGroup, Input, Label } from "reactstrap";
 import { Section, Division } from "./styles";
 import { BsExclamationCircleFill } from "react-icons/bs";
 import { colors } from "../../styles/colors";
+import { updateTask } from "../../services/task-service";
+import { useState } from "react";
 
-function TaskCard({ title = "Help my mom", date, isImportant = false, isCompleted = false }) {
+function TaskCard({ id, title, date, important = false, completed = false, handleTasks }) {
+  const [isCompleted, setIsCompleted] = useState(completed);
+  const [isImportant, setIsImportant] = useState(important);
   const color = isCompleted && isImportant ? colors.blue.light : ( isImportant ? colors.blue.normal : colors.gray.icon );
+  const dateParsed = new Date(date);
+  const [day, month, numDay] = dateParsed.toString().split(" ");
+
+  function handleChange() {
+    setIsCompleted(!isCompleted);
+    updateTask(id, { completed: !isCompleted }).then(upTask => {
+      handleTasks(tasks => {
+        const index = tasks.findIndex(task => task.id === id);
+        tasks[index] = upTask;
+        return tasks;
+      });
+    });
+  }
+
+  function handleClick() {
+    if(isCompleted) return;
+    setIsImportant(!isImportant);
+    updateTask(id, { important: !isImportant }).then(upTask => {
+      handleTasks(tasks => {
+        const index = tasks.findIndex(task => task.id === id);
+        tasks[index] = upTask;
+        return tasks;
+      });
+    });
+  }
 
   return (
       <Section>
         <FormGroup check style={{maxWidth: "240px"}}>
-          <Input type="checkbox"/>
+          <Input 
+            type="checkbox"
+            defaultChecked={isCompleted}
+            onChange={handleChange}
+          />
           <Division>
             <Label 
               check
@@ -19,7 +52,7 @@ function TaskCard({ title = "Help my mom", date, isImportant = false, isComplete
             </Label>
             {
               date
-              ? <span>Saturday</span>
+              ? <span style={{color: isCompleted ? colors.gray.normal : colors.black}}>{`${day}, ${month} ${numDay}`}</span>
               : null
             }
           </Division>
@@ -28,6 +61,7 @@ function TaskCard({ title = "Help my mom", date, isImportant = false, isComplete
           size="20px"
           style={{cursor: "pointer"}}
           color={color}
+          onClick={handleClick}
         />
       </Section>
   );
